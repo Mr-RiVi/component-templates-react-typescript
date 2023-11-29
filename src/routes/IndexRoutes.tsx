da619a34126de64161706935f05260be3b699f12
Rivindu Fernando <fernandorivindu2@gmail.com>
2023-11-30 00:12:08 +0530

    stashing

--- src/routes/IndexRoutes.tsx
+++ src/routes/IndexRoutes.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import SideNavBar from "../Pages/SideNavBar";
 import Table from "../Pages/Table";
+import DropdownMenu from "../Pages/DropdownMenu";
 
 const IndexRoutes: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const IndexRoutes: React.FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/side-nav-bars" element={<SideNavBar />} />
       <Route path="/tables" element={<Table />} />
+      <Route path="/dropdowns" element={<DropdownMenu />} />
     </Routes>
   );
 };
